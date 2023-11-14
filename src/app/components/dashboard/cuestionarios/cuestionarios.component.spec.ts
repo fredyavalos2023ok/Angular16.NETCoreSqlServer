c799d5706e45b01559a1d7424d53ede6f330095a
fredyavalos2023ok <afredy.ronaldo@gmail.com>
2023-11-14 15:13:01 -0500

    pasando a angular 12

--- src/app/components/dashboard/cuestionarios/cuestionarios.component.spec.ts
+++ src/app/components/dashboard/cuestionarios/cuestionarios.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CuestionariosComponent } from './cuestionarios.component';
 
@@ -6,7 +6,7 @@ describe('CuestionariosComponent', () => {
   let component: CuestionariosComponent;
   let fixture: ComponentFixture<CuestionariosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CuestionariosComponent ]
     })
